bdca6c0a01ec269e6e4954a35692403c2d621ece
Piotr <you@example.com>
2019-06-30 15:14:19 +0200

    refactored Bank class

--- src/Service/Bank.php
+++ src/Service/Bank.php
@@ -6,8 +6,33 @@ namespace App\Service;
 
 class Bank
 {
-    public function reduced($source, $to)
+    private $rates;
+
+    public function __construct()
     {
-        return Money::dollar(10);
+        $this->rates = [];
+    }
+
+    public function reduce($source, string $to): Money
+    {
+        if($source instanceof Money)
+        {
+            return $source;
+        }
+        if($source instanceof Sum)
+        {
+            return $source->reduced($to);
+        }
+    }
+
+    public function addRate(string $from, string $to, int $rate): void
+    {
+        $this->rates[$from] = [ $to => $rate];
+    }
+
+    public function rate(string $from ,string $to ): int
+    {
+        if ($from === $to) return 1;
+        return  $this->rates[$from][$to];
     }
 }
