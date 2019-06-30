4733a821119e703ee81469f54d4fa81ff2a0c9e1
Piotr <you@example.com>
2019-06-30 15:14:50 +0200

    added Sum class

---
+++ src/Service/Sum.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace App\Service;
+
+
+class Sum
+{
+    public $augend;
+    public $addend;
+    public $amount;
+
+    public function __construct($augend, $addend)
+    {
+        $this->augend = $augend;
+        $this->addend = $addend;
+    }
+
+    public function reduced(String $to): Money
+    {
+        $this->amount = $this->augend + $this->addend;
+        return new Money($this->amount, $to);
+    }
+}
